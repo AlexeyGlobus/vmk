47bb5b19648e556900420aa81a41d1e1f54c6797
Debian <debian@debian9.linuxvmimages.local>
2021-10-31 03:57:28 -0500

    Added Auth Controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Creates a user with the returned fields, taking care of hashing the password before
+     * 
+     */
+    public function register(Request $request)
+    {
+        $v = Validator::make($request->all(), [
+            'email' => 'required|email|unique:users',
+            'password'  => 'required|min:3|confirmed',
+        ]);
+        if ($v->fails())
+        {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $v->errors()
+            ], 422);
+        }
+        $user = new User;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        return response()->json(['status' => 'success'], 200);
+    }
+
+    /**
+     * Checks the provided credentials and returns generated token
+     * in the headers of the response if success
+     * 
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if ($token = $this->guard()->attempt($credentials)) {
+            return response()->json(['status' => 'success'], 200)->header('Authorization', $token);
+        }
+        return response()->json(['error' => 'login_error'], 401);
+    }
+
+    /**
+     * Disconnects users by disabling the token
+     * 
+     */
+    public function logout()
+    {
+        $this->guard()->logout();
+        return response()->json([
+            'status' => 'success',
+            'msg' => 'Logged out Successfully.'
+        ], 200);
+    }
+
+    /**
+     * Retrieves the logged user’s information and send it back to the response dataset
+     */
+    public function user(Request $request)
+    {
+        $user = User::find(Auth::user()->id);
+        return response()->json([
+            'status' => 'success',
+            'data' => $user
+        ]);
+    }
+
+    /**
+     * Refreshes the token if it has expired. 
+     * It is possible to define the duration of validity
+     * of the token in the file ‘config / jwt.php’.
+     */
+    public function refresh()
+    {
+        if ($token = $this->guard()->refresh()) {
+            return response()
+                ->json(['status' => 'successs'], 200)
+                ->header('Authorization', $token);
+        }
+        return response()->json(['error' => 'refresh_token_error'], 401);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    private function guard()
+    {
+        return Auth::guard();
+    }
+}
