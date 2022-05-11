e76674df27c0aad12f8eca32ad93fe2f5b4f6ed2
AlexeyAtServer <alexey.wireless@gmail.com>
2022-05-11 03:26:19 -0500

    Updated Access Right form

--- resources/js/components/access-rights/Form.vue
+++ resources/js/components/access-rights/Form.vue
@@ -38,38 +38,36 @@
 		      </thead>
 		      <tbody>
 		        <tr
-			    	v-for="(accessRight, index) in filteredAccessRights" 
-			    	:key="index"
+				    	v-for="(el, index) in filteredAccessRights" 
+				    	:key="index"
 		        >
 		          <td class="text-left">
-		          	<p 	v-if="tableNameIsChanged(accessRight.table_name)" 
-		          			class="text-uppercase font-weight-bold"
-		          	>
-			          	{{ $t(getTableName(accessRight.table_name)) }}
+		          	<p class="text-uppercase font-weight-bold">
+			          	{{ $t(getTableName(el.label)) }}
 			          </p>
 		        	</td>
-		          <td class="text-left">{{ $t(getRoleName(accessRight.role)) }}</td>
+		          <td class="text-left">{{ $t(getRoleName(el.ar.role)) }}</td>
 		          <td>
       		      <v-checkbox
-						      v-model="accessRight.rights.create"
+						      v-model="el.ar.rights.create"
 				    		>
 	    					</v-checkbox>
 		          </td>
 		          <td>
       		      <v-checkbox
-						      v-model="accessRight.rights.read"
+						      v-model="el.ar.rights.read"
 				    		>
 	    					</v-checkbox>
 		          </td> 
 		          <td>
       		      <v-checkbox
-						      v-model="accessRight.rights.update"
+						      v-model="el.ar.rights.update"
 				    		>
 	    					</v-checkbox>
 		          </td>
 		          <td>
       		      <v-checkbox
-						      v-model="accessRight.rights.delete"
+						      v-model="el.ar.rights.delete"
 				    		>
 	    					</v-checkbox>
 		          </td> 
@@ -81,7 +79,7 @@
 						      small
 					        color="#1976d2"	
 						      v-if="canEdit"
-						      @click.prevent="save(accessRight)"
+						      @click.prevent="update(el.ar)"
 						    >
 		          		<v-icon>
 				            save
@@ -93,18 +91,85 @@
 		    </template>
 		  </v-simple-table>
 	  </v-card>
-    <h3 v-else>{{ $t('403 Access denied') }}</h3>
+	  <v-expansion-panels accordion class="mt-3">
+      <v-expansion-panel>
+      <v-expansion-panel-header>Add rule</v-expansion-panel-header>
+      <v-expansion-panel-content class="pa-0">
+			  <v-simple-table>
+			    	<tbody>
+		       		<tr>
+		          <td class="text-left">
+		          	<p class="text-uppercase font-weight-bold">
+			          	dropdown table
+			          </p>
+		        	</td>
+		          <td class="text-left">dropdown role</td>
+		          <td>
+      		      <v-checkbox
+						      v-model="newAccessRight.rights.create"
+				    		>
+	    					</v-checkbox>
+		          </td>
+		          <td>
+      		      <v-checkbox
+						      v-model="newAccessRight.rights.read"
+				    		>
+	    					</v-checkbox>
+		          </td> 
+		          <td>
+      		      <v-checkbox
+						      v-model="newAccessRight.rights.update"
+				    		>
+	    					</v-checkbox>
+		          </td>
+		          <td>
+      		      <v-checkbox
+						      v-model="newAccessRight.rights.delete"
+				    		>
+	    					</v-checkbox>
+		          </td> 
+		          <td>
+        		    <v-btn
+						      class="ma-2"
+						      outlined
+						      fab
+						      small
+					        color="#1976d2"	
+						      v-if="canEdit"
+						      @click.prevent="update(newAccessRight)"
+						    >
+		          		<v-icon>
+				            save
+				          </v-icon>
+				        </v-btn>
+		          </td>
+		        </tr>
+		      </tbody>
+		  </v-simple-table>
+      </v-expansion-panel-content>
+	    </v-expansion-panel>
+	  </v-expansion-panels>
 	</div>
+	    <h3 v-else>{{ $t('403 Access denied') }}</h3>
 </template>
 
 <script>
-  let prevTable = '';
 	export default {
 		data: () => {
 			return {
 				table_name: 'access_rights',
 				isReady: false,
-				accessRightsSearch: ''
+				accessRightsSearch: '',
+				newAccessRight: {
+					table_name: '',
+					role: 0,
+					rights: {
+						create: false,
+						read: false,
+						update: false,
+						delete: false
+					}
+				}
 			}
 		},
     created() {
@@ -119,26 +184,35 @@
 				return true;
 			},
 			filteredAccessRights() {
-				return this.$store.state.accessRights.all.filter(accessRight => {
+				let filtered = this.$store.state.accessRights.all.filter(accessRight => {
 					let tn = this.$t(this.getTableName(accessRight.table_name)).toLowerCase();
 					return !!accessRight.table_name &&
 						tn.indexOf(this.accessRightsSearch.toLowerCase()) !== -1;
 				});
+				let prevTableName = '';
+				let result = [];
+				filtered.forEach(el => {
+					let res = {};
+					let label = '';
+					if (el.table_name !== prevTableName) {
+						label = el.table_name;
+					}
+					res.ar = el;
+					res.label = label;
+					prevTableName = el.table_name;
+					result.push(res);
+				});
+				return result;
 			}
 		},
 		methods: {
-			tableNameIsChanged(name) {
-				let isChanged = (name != prevTable);
-				//prevTable = name;
-				return isChanged;
-			},
 			getTableName (table) {
 				return this.$store.getters.tableName(table) + '';
 			},
 			getRoleName (role) {
 				return this.$store.getters.roleName(role) + '';
 			},
-			save(data) {
+			update(data) {
 				let rights = 0;
 				let result = Object.assign({}, data);
 				if (typeof data.rights === 'object') {
@@ -148,6 +222,7 @@
 										data.rights.delete * 8;
 				}
 				result.rights = rights;
+				console.log(result)
 			} 
 		}
 	}
@@ -156,5 +231,15 @@
 <style scoped>
 	h3, h4 {
 		color: rgba(0, 0, 0, 0.70);
+	}
+  table > tbody > tr > td:first-child, table > tbody > tr > td:nth-child(2) {
+		word-break: break-word;
+		width: 160px;
+	}
+	table > tbody > tr > td:first-child > p {
+		margin-bottom: 0;
+	}
+	.v-expansion-panel-content>>> .v-expansion-panel-content__wrap {
+  	padding: 0 !important;
 	}
 </style>
